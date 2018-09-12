7859ea19d1121ca4d53d4e37140785ab39583a1a
gurpreetsingh05222 <gurpreetsingh05222@gmail.com>
2018-09-12 13:09:11 +0530

    UPDATED FILES

--- joblister.sql
+++ joblister.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 12, 2018 at 09:11 AM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.2.3
+-- Generation Time: Sep 12, 2018 at 09:36 AM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -58,16 +58,17 @@ CREATE TABLE `jobs` (
   `salary` varchar(255) NOT NULL,
   `location` varchar(255) NOT NULL,
   `contact_user` varchar(255) NOT NULL,
-  `contact_email` varchar(255) NOT NULL
+  `contact_email` varchar(255) NOT NULL,
+  `post_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `jobs`
 --
 
-INSERT INTO `jobs` (`id`, `category_id`, `company`, `job_title`, `description`, `salary`, `location`, `contact_user`, `contact_email`) VALUES
-(1, 1, 'XYZ', 'Web developer', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac rhoncus magna. Vestibulum sem augue, iaculis ut venenatis in, rhoncus sit amet tortor. Nulla mauris leo, varius non arcu non, tempor placerat odio.', '60K', 'NYC', 'ABC', 'abc@example.com'),
-(2, 2, 'ERT', 'Programmer', 'Vestibulum tempus est non pretium sodales. Aliquam rhoncus nunc ac odio auctor, id volutpat lorem gravida. Cras felis ante, ultricies ullamcorper augue in, pulvinar condimentum nisi. Nam posuere turpis sed ipsum imperdiet pretium.', '50K', 'VBN', 'YFG', 'yfg@example.com');
+INSERT INTO `jobs` (`id`, `category_id`, `company`, `job_title`, `description`, `salary`, `location`, `contact_user`, `contact_email`, `post_date`) VALUES
+(1, 1, 'XYZ', 'Web developer', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac rhoncus magna. Vestibulum sem augue, iaculis ut venenatis in, rhoncus sit amet tortor. Nulla mauris leo, varius non arcu non, tempor placerat odio.', '60K', 'NYC', 'ABC', 'abc@example.com', '2018-09-12 07:18:30'),
+(2, 2, 'ERT', 'Programmer', 'Vestibulum tempus est non pretium sodales. Aliquam rhoncus nunc ac odio auctor, id volutpat lorem gravida. Cras felis ante, ultricies ullamcorper augue in, pulvinar condimentum nisi. Nam posuere turpis sed ipsum imperdiet pretium.', '50K', 'VBN', 'YFG', 'yfg@example.com', '2018-09-12 07:18:30');
 
 --
 -- Indexes for dumped tables
--- lib/database.php
+++ lib/database.php
@@ -8,7 +8,7 @@ class Database{
 	private $error;
 	private $stmt;
 
-	public function__construct(){
+	public function __construct(){
 		$dsn = 'mysql:host='. $this->host .';dbname='. $this->dbname;
 
 		$options = array(
@@ -24,14 +24,40 @@ class Database{
 	}
 
 	public function query($query){
-		$this->smt = $this->dbh->prepare($query);
+		$this->stmt = $this->dbh->prepare($query);
 	}
 
 	public function bind($param, $value, $type = null){
 		if(is_null($type)){
 			switch(true){
-				
+				case is_int ($value) :
+					$type = PDO::PARAM_INT;
+					break;
+				case is_bool ($value) :
+					$type = PDO::PARAM_BOOL;
+					break;
+				case is_null ($value) :
+					$type = PDO::PARAM_NULL;
+					break;
+				default :
+					$type = PDO::PARAM_STR;
 			}
 		}
+		$this->stmt->bindValue($param, $value, $type);
+
 	}
-}
+
+		public function execute(){
+			return $this->stmt->execute();
+		}
+
+		public function resultSet(){
+			$this->execute();
+			return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+		}
+
+		public  function single(){
+			$this->execute();
+			return $this->stmt->fetch(PDO::FETCH_OBJ);
+		}
+	}
