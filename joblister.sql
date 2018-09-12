fe54e4039d8efd71ef86ff05347c968ee594c985
Gurpreetsingh05222 <gurpreetsingh05222@gmail.com>
2018-09-12 12:44:16 +0530

    Mysql database file

---
+++ joblister.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 12, 2018 at 09:11 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `joblister`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Business'),
+(2, 'Technology'),
+(3, 'Retail'),
+(4, 'Construction');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+
+CREATE TABLE `jobs` (
+  `id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `company` varchar(255) NOT NULL,
+  `job_title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `salary` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `contact_user` varchar(255) NOT NULL,
+  `contact_email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `jobs`
+--
+
+INSERT INTO `jobs` (`id`, `category_id`, `company`, `job_title`, `description`, `salary`, `location`, `contact_user`, `contact_email`) VALUES
+(1, 1, 'XYZ', 'Web developer', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac rhoncus magna. Vestibulum sem augue, iaculis ut venenatis in, rhoncus sit amet tortor. Nulla mauris leo, varius non arcu non, tempor placerat odio.', '60K', 'NYC', 'ABC', 'abc@example.com'),
+(2, 2, 'ERT', 'Programmer', 'Vestibulum tempus est non pretium sodales. Aliquam rhoncus nunc ac odio auctor, id volutpat lorem gravida. Cras felis ante, ultricies ullamcorper augue in, pulvinar condimentum nisi. Nam posuere turpis sed ipsum imperdiet pretium.', '50K', 'VBN', 'YFG', 'yfg@example.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `jobs`
+--
+ALTER TABLE `jobs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
